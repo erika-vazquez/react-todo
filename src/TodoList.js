90fcd21233edb9a00463a823517becaa99274c1e
erika-vazquez <ito2016@protonmail.ch>
2019-07-09 15:21:17 -0500

    Added folders and files for this project

---
+++ src/TodoList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+class TodoList extends Component {
+  render() {
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.props.addItem}>//input items are submitted when the form is submitted
+            <input placeholder="Task" />
+            <button type="submit"> Add Task </button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+export default TodoList
